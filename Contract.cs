9a61498e71ebbf72438cd6c6c64772a504c030ea
Lucas <lukasrub@gmail.com>
2022-04-25 02:00:27 -0300

    Add files via upload

---
+++ Contract.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Deel
+{
+	public class Contract
+	{
+		public string TaxResidence { get; set; }
+		public DateTime StartDate { get; set; }
+		public Rate Rate { get; set; }
+	}
+}
---
+++ Rate.cs
@@ -0,0 +1,9 @@
+﻿namespace Deel
+{
+	public class Rate
+	{
+		public int Amount { get; set; }
+		public string Currency { get; set; }
+		public string Periodicity { get; set; }
+	}
+}
